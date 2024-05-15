3a5dc54eeccd01ebde6930762702b5b1fe9ff259
unknown <davedalimocon@gmail.com>
2024-05-16 01:58:42 +0800

    feat(Email Server):It can now send email thru gmail smtp server

--- API/Controllers/LibraryController.cs
+++ API/Controllers/LibraryController.cs
@@ -10,9 +10,11 @@ namespace API.Controllers
 public class LibraryController : ControllerBase
 {
     public Context Context {get;}
-    public LibraryController(Context context)
+    public EmailService EmailService {get;}
+    public LibraryController(Context context, EmailService emailService)
     {
         Context = context;
+        EmailService = emailService;
     }
 
     [HttpPost("register")]
@@ -24,6 +26,25 @@ public class LibraryController : ControllerBase
 
         Context.Users.Add(user);
         Context.SaveChanges();
+
+        const string subject = "Account Created";
+
+        var body = $"""
+            <html>
+                <body>
+                    <h1>Hello, {user.FirstName} {user.LastName} </h1>
+                    <h2>
+                        You accoutn has been created and we have sent approval request to admin.
+                        Once the request is approved by admin you will receive email,and you will be 
+                        able to login in to your account.
+                    </h2>
+                    <h3> Thanks </h3>
+                </body>
+            </html>
+            """;
+
+            EmailService.SendEmail(user.Email, subject, body);
+
         return Ok( @"Thank you for registering. You account has ben sent for approval. Once it is approved, you will get an email.");
     }
 
